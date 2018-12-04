046ad9e759b78ec4342a6887492c5380bac4380d
jtbamford <tom.bamford5@yahoo.co.uk>
2018-12-04 11:25:08 +0000

    added service layer

--- src/main/java/com/qa/BabyNameGenerator/service/BabyNameService.java
+++ src/main/java/com/qa/BabyNameGenerator/service/BabyNameService.java
@@ -1,5 +1,7 @@
 package com.qa.BabyNameGenerator.service;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,17 @@ public class BabyNameService implements IBabyNameService {
 	@Autowired
 	private IBabyNameRepository repo;
 	
-	public String createBabyName(int length) {
+	char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	StringBuilder word = new StringBuilder();
+	
+	public String createBabyName(int length) {	
+		Random random = new Random();
+		int letterIndex = random.nextInt(25);
+		for(int i=0;i<length;i++) {
+			word.append(letters[letterIndex]);
+		}
 		
-		// put random string generation here
-		
-		return repo.createBabyName(length);
+		return repo.createBabyName(word.toString());
 	}
 
 }
