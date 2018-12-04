e59359e73be6de9e25e024405d308efcddfb5685
jtbamford <tom.bamford5@yahoo.co.uk>
2018-12-04 10:53:11 +0000

    nearly completed

--- src/main/java/com/qa/BabyNameGenerator/persistence/domain/BabyName.java
+++ src/main/java/com/qa/BabyNameGenerator/persistence/domain/BabyName.java
@@ -1,5 +1,16 @@
 package com.qa.BabyNameGenerator.persistence.domain;
 
 public class BabyName {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
